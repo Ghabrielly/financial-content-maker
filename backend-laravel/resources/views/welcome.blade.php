a413347aa5564cb6afaaebaa510b02f4e9c7bf63
Luís Eduardo <luiseduardocass06@gmail.com>
2025-11-10 17:22:04 -0300

    feat: initial project structure

---
+++ backend-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <style>
+    .container {
+        display: grid;
+        justify-content: center;
+    }
+    </style>
+</head>
+<body>
+ <div class="container">
+     <h1>rodou legal</h1>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5_ibpbg0RX5oft1sBfn_r99R-EFC-EB6dWA&s" alt="">
+ </div>
+
+</body>
+</html>
