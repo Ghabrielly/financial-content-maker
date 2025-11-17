6f5c58d8e88408c07db726b04d695932e0329f69
Luís Eduardo <luiseduardocass06@gmail.com>
2025-11-17 14:44:59 -0300

    feat: add form request validation to gerar conteudo, reprovar conteudo and update conteudo

--- backend-laravel/app/Http/Requests/GeracaoRequest.php
+++ backend-laravel/app/Http/Requests/GeracaoRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class ConteudoRequest extends FormRequest
+class GeracaoRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,8 +23,7 @@ class ConteudoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'topic' => ['sometimes', 'required', 'string', 'min:3', 'max:255'],
-            'conteudo' => ['sometimes', 'required', 'string', 'min:20'],
+            'topic' => ['required', 'string', 'min:3', 'max:255']
         ];
     }
 }
--- backend-laravel/app/Http/Requests/ReprovacaoRequest.php
+++ backend-laravel/app/Http/Requests/ReprovacaoRequest.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class ConteudoRequest extends FormRequest
+class ReprovacaoRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -18,13 +17,12 @@ class ConteudoRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array|string>
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            'topic' => ['sometimes', 'required', 'string', 'min:3', 'max:255'],
-            'conteudo' => ['sometimes', 'required', 'string', 'min:20'],
+            'motivo_reprovacao' => ['required', 'string', 'min:10', 'max:1000'],
         ];
     }
 }
