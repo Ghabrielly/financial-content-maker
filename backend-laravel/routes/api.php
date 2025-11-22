7d3edcb89132d3d09dc13eeafae664ccbfd03b50
Luís Eduardo <luiseduardocass06@gmail.com>
2025-11-22 03:23:09 -0300

    feat: add login, register and logout routes, also middleware to protected routes

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -1,28 +1,45 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TokenController;
 use App\Http\Controllers\ConteudoController;
 
+/*
+ * Routes to manage authentication (login, register).
+ *
+ */
+Route::post('/login', [TokenController::class, 'issue'])
+    ->name('token.issue');
+Route::post('/register', [UserController::class, 'store'])
+    ->name('user.store');
 
-Route::post('/conteudos/gerar', [ConteudoController::class, 'gerar'])
-    ->name('conteudos.gerar');
+/*
+ * Routes to manage "conteudos".
+ *
+ */
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::get('/conteudos', [ConteudoController::class, 'index'])
-    ->name('conteudos.index');
+    Route::post('/logout', [TokenController::class, 'destroy'])->name('token.destroy');
 
-Route::get('/conteudos/{conteudo}', [ConteudoController::class, 'show'])
-    ->name('conteudos.show');
+    Route::post('/conteudos/gerar', [ConteudoController::class, 'gerar'])
+        ->name('conteudos.gerar');
 
-Route::put('/conteudos/{conteudo}', [ConteudoController::class, 'update'])
-    ->name('conteudos.update');
+    Route::get('/conteudos', [ConteudoController::class, 'index'])
+        ->name('conteudos.index');
 
-Route::delete('/conteudos/{conteudo}', [ConteudoController::class, 'destroy'])
-    ->name('conteudos.destroy');
+    Route::get('/conteudos/{conteudo}', [ConteudoController::class, 'show'])
+        ->name('conteudos.show');
 
-Route::post('/conteudos/{conteudo}/aprovar', [ConteudoController::class, 'aprovar'])
-    ->name('conteudos.aprovar');
+    Route::put('/conteudos/{conteudo}', [ConteudoController::class, 'update'])
+        ->name('conteudos.update');
 
-Route::post('/conteudos/{conteudo}/reprovar', [ConteudoController::class, 'reprovar'])
-    ->name('conteudos.reprovar');
+    Route::delete('/conteudos/{conteudo}', [ConteudoController::class, 'destroy'])
+        ->name('conteudos.destroy');
+
+    Route::post('/conteudos/{conteudo}/aprovar', [ConteudoController::class, 'aprovar'])
+        ->name('conteudos.aprovar');
+
+    Route::post('/conteudos/{conteudo}/reprovar', [ConteudoController::class, 'reprovar'])
+        ->name('conteudos.reprovar');
+});
